9b0f68fa8a2fea00433b046e145952661a396170
jiyoon-lee <jiyoon3421@gmail.com>
2022-11-03 08:35:34 +0900

    commit

--- src/javascripts/components/PlayView/index.js
+++ src/javascripts/components/PlayView/index.js
@@ -1,6 +1,6 @@
 export default class PlayView {
   constructor() {
-    this.rootElement = PlayView.createRenderElement()
+    this.renderElement = PlayView.createRenderElement()
   }
   static createRenderElement() {
     const playViewWrapper = document.createElement('article')
@@ -11,11 +11,20 @@ export default class PlayView {
 
   }
   renderMovieContainer() {
-    const { id: { videoId }, snippet: { channelTitle, description, publishTime, thumbnails: { default: { url } }, title } } = this.movieInfo
-    this.rootElement.innerHTML = `
-      <iframe id="player" type="text/html" width="640" height="360"
-      src="http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://example.com"
-      frameborder="0"></iframe>
+    const { id: { videoId }, snippet: { channelTitle, description, publishTime, title } } = this.movieInfo
+    this.renderElement.innerHTML = `
+    <div class="play-view-roof">
+      <img width="100" src="assets/images/youtube_logo.png" />
+    </div>
+    <iframe id="player" type="text/html" height="200"
+    src="http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://example.com"
+    frameborder="0"></iframe>
+    <div class="view-info-container">
+      <div class="info-title">${title}</div>
+      <div class="info-description">${description}</div>
+      <div class="info-channel-title">${channelTitle}</div>
+      <div class="info-publish-time">${publishTime}</div>
+    </div>
     `
   }
   playMovie(payload) {
@@ -25,8 +34,9 @@ export default class PlayView {
     this.renderMovieContainer()
   }
   show() {
-    console.log(document.body)
-    document.body.innerHTML = this.rootElement
+    const rootElement = document.querySelector('#app')
+    const $article = document.querySelector('article')
+    rootElement.replaceChild(this.renderElement, $article)
   }
   on() {
 
@@ -35,6 +45,6 @@ export default class PlayView {
 
   }
   render() {
-    return this.rootElement
+    return this.renderElement
   }
 }
--- src/javascripts/components/index.js
+++ src/javascripts/components/index.js
@@ -4,4 +4,3 @@ export { default as PlayView } from './PlayView/index.js';
 export { default as SearchView } from './SearchView/index.js';
 export { default as TabButtons } from './TabButtons/index.js';
 export { default as TopVideo } from './TopVideo/index.js';
-export { default as PlayView } from './PlayView/index.js';
